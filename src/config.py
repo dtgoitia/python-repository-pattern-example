0acc0fa385fdb920c92300806bc9441e72fbf112
David Torralba Goitia <david.torralba@babylonhealth.com>
2021-03-19 11:35:07 +0000

    Support config

---
+++ src/config.py
@@ -0,0 +1,20 @@
+import os
+from typing import Optional
+
+import attr
+
+
+@attr.s(auto_attribs=True, frozen=True)
+class AppConfig:
+    database_uri: str
+    debug: bool
+
+
+_CACHED_CONFIG: Optional[AppConfig] = None
+
+
+def get_config() -> AppConfig:
+    if _CACHED_CONFIG:
+        return _CACHED_CONFIG
+
+    return AppConfig(database_uri=os.environ["DATABASE_URI"], debug=True)
